60339dc8d232b3f031dd8212585d52dc4d66c101
Justin Bauguess <jmbauguess@gmail.com>
2013-01-31 20:05:11 -0500

    changed the table structures (put the constraints in with the create statements) added basic_inserts.sql, which does inserts for all the tables with test data

--- growler_tables.sql
+++ growler_tables.sql
@@ -4,30 +4,23 @@ Justin Bauguess 1/29/13
 */
 /* drop table theme; */
 create table theme (
-	theme_id			varchar(4)
+	theme_id			int(4)			primary key
 	,theme_name			varchar(30)
-	,theme_description	varchar(250)
-	,theme_creator		varchar(40)
+	,theme_description		varchar(250)
+	,theme_creator			varchar(40)
 	,theme_year			year(4)
 	,visible			boolean
 	,active				boolean
 	);
 	
-alter table theme
-add constraint theme_pk primary key (theme_id);
-
 /*
 Creates the table for storing user information
 Justin Bauguess 1/29/13
 */	
 /* drop table users; */
 create table users (
-	user_id				varchar(40)
-	,password			varchar(50)
+	user_id				varchar(40)		primary key
 	);
-
-alter table users
-add constraint users_pk primary key (user_id);
 
 /*
 Bridge table for the users and themes, designed to help keep track of ranks
@@ -35,22 +28,12 @@ Justin Bauguess 1/29/13
 */	
 /* drop table theme_ranking; */
 create table theme_ranking (
-	user_id				varchar(40)
-	,theme_id			varchar(4)
-	,theme_rank			int
+	user_id				varchar(40)		
+	,theme_id			int(4)			
+	,theme_rank			int			check (theme_rank > 0 and theme_rank < 11)
+	,constraint foreign key (user_id) references users(user_id)
+	,constraint foreign key (theme_id) references theme(theme_id)
 	);
-
-alter table theme_ranking
-add constraint theme_ranking_user_pk primary key (user_id, theme_id);
-
-alter table theme_ranking
-add constraint theme_ranking_user_fk foreign key (user_id) references users(user_id);
-
-alter table theme_ranking
-add constraint theme_ranking_theme_fk foreign key (theme_id) references theme(theme_id);
-
-alter table theme_ranking
-add constraint rank_check check (theme_rank >0 and theme_rank <11);
 
 /*
 Creates the table for storing speaker information
@@ -58,16 +41,9 @@ Justin Bauguess 1/29/13
 */
 /* drop table speaker; */
 create table speaker (
-	speaker_id			varchar(4)
-	,speaker_first		varchar(30)
-	,speaker_last		varchar(30)
-	,suggested_by		varchar(40)
-	,speaking_since		year(4)
+	speaker_id			int(4)			primary key
+	,speaker_first			varchar(30)
+	,speaker_last			varchar(30)
+	,suggested_by			varchar(40)		
+	,constraint foreign key (suggested_by) references users(user_id)
 	);
-	
-alter table speaker
-add constraint speaker_pk primary key (speaker_id);
-
-alter table speaker
-add constraint speaker_suggested_fk foreign key (suggested_by) references users(user_id);
-
