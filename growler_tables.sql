8942fcc74986d812d4dcd9deb87b621f6cd1e877
fiskr <bjf092@gmail.com>
2013-02-05 14:52:59 -0500

    Made changes to the tables: users is now user, user_id is now id and similar changes in other tables, constraints were moved into the definitions of the attributes, drop statements were uncommented so the script will always drop a table before creating it, and other formatting changes. I did not change the commas in the table attribute definitions, though I thought of doing so on account of formatting.

--- growler_tables.sql
+++ growler_tables.sql
@@ -1,13 +1,18 @@
 /*
 Creates the table for storing theme information
 Justin Bauguess 1/29/13
+edited:
+Brandon Foster 02/05/13
 */
-/* drop table theme; */
-create table theme (
+
+--dropping tables before we create them
+DROP TABLE theme; 
+
+CREATE TABLE theme (
 	theme_id			int			primary key
 	,theme_name			varchar(30)
 	,theme_description		varchar(250)
-	,theme_creator			varchar(40)
+	,theme_creator			int		references user(id)
 	,theme_year			year(4)
 	,visible			boolean
 	,active				boolean
@@ -17,33 +22,30 @@ create table theme (
 Creates the table for storing user information
 Justin Bauguess 1/29/13
 */	
-/* drop table users; */
-create table users (
-	user_id				varchar(40)		primary key
+DROP TABLE user; 
+CREATE TABLE user (
+	id			int		primary key
 	);
 
 /*
 Bridge table for the users and themes, designed to help keep track of ranks
 Justin Bauguess 1/29/13
 */	
-/* drop table theme_ranking; */
-create table theme_ranking (
-	user_id				int		
-	,theme_id			int(4)			
-	,theme_rank			int			check (theme_rank > 0 and theme_rank < 11)
-	,constraint foreign key (user_id) references users(user_id)
-	,constraint foreign key (theme_id) references theme(theme_id)
+DROP TABLE theme_ranking;
+CREATE TABLE theme_ranking (
+	user_id				int		REFERENCES user(id)	
+	,theme_id			int(4)		REFERENCES theme(id)	
+	,theme_rank			int		CHECK (theme_rank > 0 AND theme_rank < 11)
 	);
 
 /*
 Creates the table for storing speaker information
 Justin Bauguess 1/29/13
 */
-/* drop table speaker; */
-create table speaker (
-	speaker_id			int			primary key
-	,speaker_first			varchar(30)
-	,speaker_last			varchar(30)
-	,suggested_by			varchar(40)		
-	,constraint foreign key (suggested_by) references users(user_id)
+DROP TABLE speaker; 
+CREATE TABLE speaker (
+	 id				int			PRIMARY KEY
+	,first_name			varchar(30)
+	,last_name			varchar(30)
+	,suggested_by			int			REFERENCES user(id)		
 	);
