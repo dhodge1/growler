e4d708ddd0295f76879a1b5c5c31d2b48176dea4
fiskr <bjf092@gmail.com>
2013-02-06 20:44:48 -0500

    Updated theme table's insert script, removed the definition of attributes the values are being assigned to (so that the names of those attributes can change in the create script without having to go change them in places like the insert script), and made changes like making the ids ints instead of varchars and making user_id compatible with the user table's id attribute (essentially matching domains here).

--- db/theme_inserts.sql
+++ db/theme_inserts.sql
@@ -1,11 +1,6 @@
 INSERT INTO theme VALUES (1, "Cloud Computing", "All things Cloud, from IaaS, PaaS, DaaS, SaaS, to hosting providers, brokers, and cloud-enabling appliances", 2023, "2013", true, true);
-insert into theme (theme_id, theme_name, theme_description, theme_creator, theme_year, active, visible) 
-values ("0002", "Development Frameworks", "Any type of development framework, regardless of language", "DEFAULT", "2013", true, true);
-insert into theme (theme_id, theme_name, theme_description, theme_creator, theme_year, active, visible) 
-values ("0003", "Software Process/Lifecycle", "Waterfall, Agile, Scrum, Kanban, process improvements, new techniques", "DEFAULT", "2013", true, true);
-insert into theme (theme_id, theme_name, theme_description, theme_creator, theme_year, active, visible) 
-values ("0004", "", "Topics related to mobile computing in the enterprise, including mobile apps, phones, tablets, and other devices", "DEFAULT", "2013", true, true);
-insert into theme (theme_id, theme_name, theme_description, theme_creator, theme_year, active, visible) 
-values ("0005", "Social and Collaboration", "Tools and Techniques that make the enterprise more social and allow people to better communicate and collaborate when they are not in the same room, floor, building, city, state, or country", "DEFAULT", "2013", true, true);
-insert into theme (theme_id, theme_name, theme_description, theme_creator, theme_year, active, visible) 
-values ("0006", "Show and Tell", "Show and Tell", "DEFAULT", "2013", true, true);
+INSERT INTO theme VALUES (2, "Development Frameworks", "Any type of development framework, regardless of language", 2023, "2013", true, true);
+INSERT INTO theme VALUES (3, "Software Process/Lifecycle", "Waterfall, Agile, Scrum, Kanban, process improvements, new techniques", 2023, "2013", true, true);
+INSERT INTO theme VALUES (4, "Mobility", "Topics related to mobile computing in the enterprise, including mobile apps, phones, tablets, and other devices", 2023, "2013", true, true);
+INSERT INTO theme VALUES (5, "Social and Collaboration", "Tools and Techniques that make the enterprise more social and allow people to better communicate and collaborate when they are not in the same room, floor, building, city, state, or country", 2023, "2013", true, true);
+INSERT INTO theme VALUES (6, "Show and Tell", "Show and Tell (Description)", 2023, "2013", true, true);
