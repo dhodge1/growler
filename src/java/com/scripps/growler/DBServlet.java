73ddb8454bc3a70b7f95f20046d5dcce2bcf6d61
christophertupps <christophertupps@gmail.com>
2013-02-28 14:09:34 -0500

    Test from netbeans

--- src/java/com/scripps/growler/DBServlet.java
+++ src/java/com/scripps/growler/DBServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
+ * test Christopher Tupps
  * @author Robert Brown
  */
 @WebServlet(name = "DBServlet", urlPatterns = {"/DBServlet"})
