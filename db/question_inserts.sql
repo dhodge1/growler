9456d62e705c3c2e650623c5d9e4365cb81d9161
Fiskr <bjf092@gmail.com>
2013-03-20 06:57:51 -0400

    Minor change- removed attribute specifications from inserts and added a comment for convention's sake. Otherwise, this is the first script to work first try. WOO HOOgit add question_inserts.sql

--- db/question_inserts.sql
+++ db/question_inserts.sql
@@ -1,4 +1,5 @@
-insert into question (id, text) values (1, "This session met my expectations:");
-insert into question (id, text) values (2, "The speaker was knowledgable on the topic:");
-insert into question (id, text) values (3, "The speaker's presentation skills were good:");
-insert into question (id, text) values (4, "The facility was appropriate for the presentation:");
+/* Inserts the questions */
+insert into question values (1, "This session met my expectations:");
+insert into question values (2, "The speaker was knowledgable on the topic:");
+insert into question values (3, "The speaker's presentation skills were good:");
+insert into question values (4, "The facility was appropriate for the presentation:");
