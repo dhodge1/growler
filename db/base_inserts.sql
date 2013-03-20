892048b2cd8252b5a22497a0bdff762386a4da9a
Fiskr <bjf092@gmail.com>
2013-03-20 15:22:49 -0400

    Question inserts had to be modified to match the number of attributes. I also noticed the script will get caught if the tables don't already exist (drop statements come first- so maybe a first build script and a refresh script will be coming our way soon

--- db/base_inserts.sql
+++ db/base_inserts.sql
@@ -76,10 +76,10 @@ fields terminated by ','
 ignore 1 lines;
 
 /* Inserts the questions */
-insert into question values (1, "This session met my expectations:");
-insert into question values (2, "The speaker was knowledgable on the topic:");
-insert into question values (3, "The speaker's presentation skills were good:");
-insert into question values (4, "The facility was appropriate for the presentation:");
+insert into question values (1, "This session met my expectations:", '2012');
+insert into question values (2, "The speaker was knowledgable on the topic:", '2012');
+insert into question values (3, "The speaker's presentation skills were good:", '2012');
+insert into question values (4, "The facility was appropriate for the presentation:", '2012');
 
 /* 
  * inserts locations
