5fb422b4d93413771e6ccb00985ccc847c6a00f7
fiskr <bjf092@gmail.com>
2013-03-27 16:19:43 -0400

    changed a join of the bridge to two subqueries referencing the bridge

--- db/create_ranks_2012.sql
+++ db/create_ranks_2012.sql
@@ -1,3 +1,10 @@
-/* This file creates the table based on last year's data which can be modified by the administrator later */
-drop table ranks_2012;
-create table ranks_2012 as (select avg(r.ranking) as rating, s.id, ceiling(count(r.session_id)/4) as count from session_ranking r, speaker s, speaker_team t where t.session_id = r.session_id and t.speaker_id = s.id group by s.id);
+/* This file creates the table based on last year's data
+ * which can be modified by the administrator later 
+ */
+DROP TABLE ranks_2012;
+CREATE TABLE ranks_2012 as (
+select avg(r.ranking) as rating
+,s.id as id
+,ceiling(count(r.session_id)/4) as count
+FROM session_ranking r, speaker s
+WHERE r.session_id IN (select session_id from speaker_team) and s.id IN (select speaker_id from speaker_team) group by s.id);
