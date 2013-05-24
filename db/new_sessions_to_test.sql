91eed527bd4a8670ba524f74584dcabf74cf3c4e
Justin Bauguess <jmbauguess@gmail.com>
2013-05-24 13:35:19 -0400

    deleted files; preparing to move to Subversion
    Bye, Brandon!

--- db/new_sessions_to_test.sql
+++
@@ -1,4 +0,0 @@
-insert into session (id, name, session_date, start_time, duration) 
-values (36,'My Session','2013-04-18', '13:00:00', 25)
-,(37,'Your Session','2013-04-18', '13:00:00', 25)
-,(38,'Our Session','2013-04-18', '13:00:00', 25);
--- db/user_insert.sql
+++
@@ -1,4 +0,0 @@
-/*
- * Inserts the default user.  This user is typically associated with last year's data.
- */
-insert into user (id) values (2023);
