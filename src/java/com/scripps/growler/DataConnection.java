2ee48086e17704d0fb36e0a9167f16a171ef5f60
Justin Bauguess <jmbauguess@gmail.com>
2013-08-09 09:12:50 -0400

    Prod database

--- src/java/com/scripps/growler/DataConnection.java
+++ src/java/com/scripps/growler/DataConnection.java
@@ -38,7 +38,7 @@ public class DataConnection {
     public Connection sendConnection() throws SQLException, ClassNotFoundException {
         logger.info("Getting the Driver, then Returning a connection using the class constants of DBNAME, DBUSER, DBPASSWORD");
         Class.forName("com.mysql.jdbc.Driver");
-        return (connection = DriverManager.getConnection("jdbc:mysql://" + AMAZON + ":3306/" + DBNAME, DBUSER, DBPASS));
+        return (connection = DriverManager.getConnection("jdbc:mysql://" + PRODUCTION + ":3306/" + DBNAME, PRODUSER, PRODPASS));
     }
 
     public String bytesToHex(byte[] b) {
