ce09e1e38519ff087e9cce2a22c4c5cde798b2df
cngrindstaff <chelseagrindstaff@yahoo.com>
2014-04-05 16:53:14 -0400

    added methods

--- src/java/com/scripps/growler/AttendeePersistence.java
+++ src/java/com/scripps/growler/AttendeePersistence.java
@@ -6,6 +6,12 @@ import java.sql.*;
 
 public class AttendeePersistence extends GrowlerPersistence {
     
+    /**
+     * Sorts queries by name ascending
+     */
+    final String SORT_BY_NAME_ASC = " order by session.name asc";
+    
+    
     // An array list object for each method to use
     ArrayList<Attendees> attendees = new ArrayList<Attendees>();
     
@@ -13,12 +19,45 @@ public class AttendeePersistence extends GrowlerPersistence {
 
 
 
-//Default Constructor 
-public AttendeePersistence() {
+    //Default Constructor 
+    public AttendeePersistence() {
+        }
+
+
+    /**
+     * Gets a list of all sessions in the database
+     *
+     * @param sort the criteria to sort the sessions
+     * @return A list of sessions in the database, sorted
+     */
+    public ArrayList<Attendees> getAllSessions(String sort) {
+        try {
+            initializeJDBC();
+            statement = connection.prepareStatement("select * from session " + sort);
+            result = statement.executeQuery();
+            attendees = new ArrayList<Attendees>();
+            while (result.next()) {
+                //Create a new Session and add all data about the session to it
+                Attendees a = new Attendees();
+                a.setId(result.getInt("session_id"));
+                a.setLocalAttendees(result.getInt("local_Attendees"));
+                a.setRemoteAttendees(result.getInt("remote_Attendees"));
+
+                //Add the session to the list
+                attendees.add(a);
+            }
+            closeJDBC();
+            return attendees;
+        } catch (Exception e) {
+        }
+        finally {
+            closeJDBC();
+        }
+        return attendees;
     }
-
-
-   public ArrayList<Attendees> getLocalAttendeesById(String id) {
+    
+    
+    public ArrayList<Attendees> getLocalAttendeesById(String id) {
         ArrayList<Attendees> attendeeList = new ArrayList<Attendees>();
         try {
             initializeJDBC();
@@ -57,4 +96,28 @@ public AttendeePersistence() {
         }
         return attendeeList;
     }         
-   }
+   
+
+    public void addAttendees(int session, int localAttendees, int remoteAttendees) {
+        try {
+            initializeJDBC();
+            statement = connection.prepareStatement("insert into attendees (session_id, local_attendees, remote_attendees) values (?, ?, ?)");
+            statement.setInt(1, session);
+            statement.setInt(2, localAttendees);
+            statement.setInt(3, remoteAttendees);
+            statement.execute();
+            closeJDBC();
+        } catch (Exception e) {
+        }
+        finally {
+            closeJDBC();
+        }
+    }
+
+
+
+
+
+
+
+}
